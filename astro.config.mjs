2f71c5a707c47a7cc3362b0e4e82cc8e08e21c5a
teovu <tvu1@st-andrews.ac.uk>
2023-12-06 08:01:38 +0000

    Added AlgoliaDocSearch client-side only component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    react(),
   ],
 });
